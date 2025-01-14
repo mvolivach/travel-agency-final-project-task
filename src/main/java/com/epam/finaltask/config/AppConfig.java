4f5ac3c2f2b500eef4f2be211d55a1ee6d91cef6
mvolivach <mvolivach2003@gmail.com>
2025-01-14 18:34:35 +0200

    code refactored

--- src/main/java/com/epam/finaltask/config/AppConfig.java
+++ src/main/java/com/epam/finaltask/config/AppConfig.java
@@ -1,18 +1,9 @@
 package com.epam.finaltask.config;
 
-import jakarta.validation.Validator;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class AppConfig {
